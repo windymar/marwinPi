5d3f8ed737491e5156120426c981488ceb756118
Marcin Windys <marcin.windys@gmail.com>
2018-09-30 18:54:41 +0200

    Added more tests to Diode RGB

--- Diodes/Tests/MarwinPi_DiodeRgbTestSuite.cpp
+++ Diodes/Tests/MarwinPi_DiodeRgbTestSuite.cpp
@@ -34,4 +34,34 @@ TEST_F(DiodeRgbTestSuite, shouldWriteHighToRedPortWhenSwitchOnRed)
     m_sut.switchOnRed();
 }
 
+TEST_F(DiodeRgbTestSuite, shouldWriteHighToGreenPortWhenSwitchOnGreen)
+{
+    EXPECT_CALL(*m_greenPortRawPtr, write(GpioValue::GpioValue_High));
+    m_sut.switchOnGreen();
+}
+
+TEST_F(DiodeRgbTestSuite, shouldWriteHighToBluePortWhenSwitchedOnBlue)
+{
+    EXPECT_CALL(*m_bluePortRawPtr, write(GpioValue::GpioValue_High));
+    m_sut.switchOnBlue();
+}
+
+TEST_F(DiodeRgbTestSuite, shouldWriteLowToRedPortWhenSwitchOffRed)
+{
+    EXPECT_CALL(*m_redPortRawPtr, write(GpioValue::GpioValue_Low));
+    m_sut.switchOffRed();
+}
+
+TEST_F(DiodeRgbTestSuite, shouldWriteLowToGreenPortWhenSwitchOffGreen)
+{
+    EXPECT_CALL(*m_greenPortRawPtr, write(GpioValue::GpioValue_Low));
+    m_sut.switchOffGreen();
+}
+
+TEST_F(DiodeRgbTestSuite, shouldWriteLowToBluePortWhenSwitchedOffBlue)
+{
+    EXPECT_CALL(*m_bluePortRawPtr, write(GpioValue::GpioValue_Low));
+    m_sut.switchOffBlue();
+}
+
 }
